d77b20d0a2ec7b7fda2fdc05a1e2ce285f21cb96
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-15 10:11:34 +0300

    Scaffold method

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@
 import 'package:flutter/material.dart';
 
 //it runs the runApp() function, passing in an instance of MyApp as an argument.
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
+
 /*StatelessWidget is a Flutter class that defines a widget that doesn't depend on any mutable state.  
 build() method, it returns a MaterialApp widget, which is a widget that sets up the basic structure and design of a Material Design app.
  we use the @override annotation to indicate that we are overriding the build() method from the StatelessWidget class.*/
@@ -12,7 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my body'),
+      ),
     );
   }
 }
