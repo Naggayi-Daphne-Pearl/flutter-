f98318407cb9215392aac5e77edca745f364a1b4
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-15 09:49:25 +0300

    Comments to understand what my code is doing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+// This line imports the material.dart library from the flutter package, which contains all of the widgets and tools needed to create Material Design applications in Flutter.
 import 'package:flutter/material.dart';
 
+//it runs the runApp() function, passing in an instance of MyApp as an argument.
 void main() {
   runApp(MyApp());
 }
-
+/*StatelessWidget is a Flutter class that defines a widget that doesn't depend on any mutable state.  
+build() method, it returns a MaterialApp widget, which is a widget that sets up the basic structure and design of a Material Design app.
+ we use the @override annotation to indicate that we are overriding the build() method from the StatelessWidget class.*/
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello World'),
