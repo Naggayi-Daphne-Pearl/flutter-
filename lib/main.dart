9337e3b5be6ddde8edd791ceff041032ca595e5f
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-15 12:19:30 +0300

    Add Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,24 @@ void main() => runApp(MyApp());
 build() method, it returns a MaterialApp widget, which is a widget that sets up the basic structure and design of a Material Design app.
  we use the @override annotation to indicate that we are overriding the build() method from the StatelessWidget class.*/
 class MyApp extends StatelessWidget {
+  void answerQuestions() {
+    print('Answered question');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['What is your favorite color', 'What is your name'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my body'),
+        body: Column(
+          children: [
+            ElevatedButton(onPressed: null, child: Text('Answer this 1')),
+            ElevatedButton(onPressed: null, child: Text('Answer this 2 ')),
+          ],
+        ),
       ),
     );
   }
