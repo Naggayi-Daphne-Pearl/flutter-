e2b270bb12ae49e495e7975098d884707ecba14a
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-16 09:20:34 +0300

    Change State of a Property with Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,29 @@ void main() => runApp(MyApp());
 /*StatelessWidget is a Flutter class that defines a widget that doesn't depend on any mutable state.  
 build() method, it returns a MaterialApp widget, which is a widget that sets up the basic structure and design of a Material Design app.
  we use the @override annotation to indicate that we are overriding the build() method from the StatelessWidget class.*/
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
   void answerQuestions() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print('Answered question');
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['What is your favorite color', 'What is your name'];
+    var questions = [
+      'What is your favorite color',
+      'What is your favourite animal'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -25,8 +40,11 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            ElevatedButton(onPressed: null, child: Text('Answer this 1')),
-            ElevatedButton(onPressed: null, child: Text('Answer this 2 ')),
+            Text(questions[questionIndex]),
+            ElevatedButton(
+                onPressed: answerQuestions, child: Text('Answer')),
+            ElevatedButton(
+                onPressed: answerQuestions, child: Text('Answer')),
           ],
         ),
       ),
